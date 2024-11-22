0b226a3107b646cee5d765b4ea339db0670b4d44
Evgeny Mangasaryan <mangasaryan.ep@gmail.com>
2024-11-22 22:07:56 +0400

    feat(backend): Filter out undefined place columns

--- backend/parser/src/parser/main.py
+++ backend/parser/src/parser/main.py
@@ -6,22 +6,30 @@ from pypdf import PdfReader
 from parser import util
 
 PAGE_NUM = r"Стр\.\s*\d+\s*из\s*\d+"
+SPORT_KIND = r"([А-Я\s]+)Основной\s+состав\s"
+RESERVE_SECTION = r"Молодежный\s+\(резервный\)\s+состав\s"
 ID = r"\d{10,}"
-ROW_START = rf"\n(?={ID}\s+)"
+ROW_START = rf"\s(?={ID}\s+)"
 COMPETITORS_NUMBER = r"\s+(?=\d+$)"
 COMPETITION_TITLE_BEFORE = r"\s+"
 LOWERCASE_RUS = r"[а-я]"
 COMPETITION_TITLE_AFTER = rf"(?=\s+{LOWERCASE_RUS})"
 DATE = r"\d{2}\.\d{2}\.\d{4}"
-DATES_BEFORE = rf"\n(?={DATE}\n{DATE})"
-DATES_AFTER = rf"(?<={DATE}\n{DATE})\n"
+DATES_BEFORE = rf"\s(?={DATE}\s{DATE})"
+UNDEFINED_REGION = r"\s?ПО\s+НАЗНАЧЕНИЮ\s?"
+DATES_AFTER = rf"(?<={DATE}\s{DATE})\s"
 UPPERCASE_RUS = r"[А-Я]"
-DISCIPLINE_BEFORE = rf"\n?(?={UPPERCASE_RUS}|$)"
+DISCIPLINE_BEFORE = rf"\s?(?={UPPERCASE_RUS}|$)"
 CITY_BEFORE = r"(?:,\s)|$"
 
 
 def remove_page_numbers(s: str) -> str:
     return re.sub(PAGE_NUM, "", s)
+
+
+def split_into_sport_kinds(s: str) -> list[str]:
+    s = re.sub(RESERVE_SECTION, "", s)
+    return list(map(str.strip, re.split(SPORT_KIND, s)))[1:]
 
 
 def split_rows(s: str) -> pd.DataFrame:
@@ -55,7 +63,7 @@ def competition_title(df: pd.DataFrame) -> pd.DataFrame:
     )
 
     cleaned_title = pd.DataFrame(
-        df["Title"].apply(lambda s: re.sub(r"\s+", " ", s.strip()))
+        df["Title"].apply(lambda s: re.sub(r"\s+", " ", s))
     )
     cleaned_rest = pd.DataFrame(df["Raw"].apply(lambda s: s.strip()))
 
@@ -87,21 +95,31 @@ def dates(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def group(df: pd.DataFrame) -> pd.DataFrame:
-    return util.flat_apply(
+    df = util.flat_apply(
         df,
         "Raw Group",
         lambda s: re.split(DISCIPLINE_BEFORE, s, maxsplit=1),
         columns=["Group", "Raw Discipline"],
     )
+    df["Group"] = df["Group"].apply(lambda s: re.sub(r"\s+", " ", s))
+    return df
 
 
 def country(df: pd.DataFrame) -> pd.DataFrame:
-    return util.flat_apply(
+    df["Raw Place"] = df["Raw Place"].apply(str.strip)
+    df = df[df["Raw Place"] != "ПО НАЗНАЧЕНИЮ"]
+    for place in df["Raw Place"].to_list():
+        print(place)
+        print()
+
+    df = util.flat_apply(
         df,
         "Raw Place",
         lambda s: re.split("\n+", s, maxsplit=1),
         columns=["Country", "Raw Region"],
     )
+    df["Country"] = df["Country"].apply(lambda s: re.sub(r"\s+", " ", s))
+    return df
 
 
 def region(df: pd.DataFrame) -> pd.DataFrame:
@@ -111,6 +129,8 @@ def region(df: pd.DataFrame) -> pd.DataFrame:
         lambda s: re.split(CITY_BEFORE, s, maxsplit=1),
         columns=["Region", "City"],
     )
+    df["Region"] = df["Region"].apply(lambda s: re.sub(r"\s+", " ", s))
+
     df["City Copy"] = df["City"].copy()
     df["City"] = df.apply(
         lambda row: row["Region"] if row["City"] == "" else row["City"],
@@ -126,29 +146,37 @@ def region(df: pd.DataFrame) -> pd.DataFrame:
 
 def main():
     reader = PdfReader("input.pdf")
-    page = reader.pages[64]
+    pages = reader.pages[86:95]
 
-    res = page.extract_text()
+    res = "\n".join(page.extract_text() for page in pages)
     pipeline = [
         remove_page_numbers,
+        split_into_sport_kinds,
+    ]
+    for step in pipeline:
+        res = step(res)
+
+    parsed_sports = {}
+    sports = {
+        key: value for key, value in zip(res[::2], res[1::2], strict=True)
+    }
+    pipeline = [
         split_rows,
         competitors_number,
         competition_title,
         dates,
         group,
         country,
-        region,
+        # region,
     ]
-    for step in pipeline:
-        res = step(res)
-    df = res
+    for key in sports:
+        res = sports[key]
+        for step in pipeline:
+            res = step(res)
+        parsed_sports[key] = res
 
-    print(df.info())
-    for col in df:
-        print(df[col].head())
-
-    for raw in df["Country"]:
-        print(repr(raw))
-
-    for raw in df["Group"]:
-        print(repr(raw))
+    for key in parsed_sports:
+        df = parsed_sports[key]
+        print(key)
+        print(df.head())
+        print()
