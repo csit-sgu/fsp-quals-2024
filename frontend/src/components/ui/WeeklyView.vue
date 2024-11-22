1ac757df71c0806a4f38d137c7395a44f9677c56
Andrew Guschin <guschin@altlinux.org>
2024-11-23 02:14:04 +0400

    fix(frontend): fix wrong layout of columns in week view

--- frontend/src/components/ui/WeeklyView.vue
+++ frontend/src/components/ui/WeeklyView.vue
@@ -18,10 +18,8 @@ const renderEvents = events.map(({ duration, startDay }) => {
   const startDayIdx = weekdays.indexOf(startDay)
   const clampedDuration = Math.min(duration, weekdays.length - startDayIdx)
   return {
-    classes: [
-      'col-start-' + (startDayIdx + 1).toString(),
-      'col-span-' + clampedDuration.toString(),
-    ],
+    classes: ['colspan' + clampedDuration.toString()],
+    startDays: startDayIdx,
     tailDays: weekdays.length - clampedDuration - startDayIdx,
   }
 })
@@ -33,13 +31,39 @@ const renderEvents = events.map(({ duration, startDay }) => {
       <div v-for="weekday in weekdays" class="min-w-8 justify-self-center font-bold text-lg">
         {{ weekday }}
       </div>
-      <Separator class="col-span-7" />
+      <Separator class="colspan7" />
 
-      <template v-for="{ classes, tailDays } in renderEvents">
+      <template v-for="{ classes, startDays, tailDays } in renderEvents">
+        <div v-if="startDays > 0" :class="'colspan' + startDays.toString()"></div>
         <WeeklyEvent :class="cn('mx-2', ...classes)" />
-        <div v-if="tailDays > 0" :class="'col-span-' + tailDays.toString()"></div>
+        <div v-if="tailDays > 0" :class="'colspan' + tailDays.toString()"></div>
       </template>
     </div>
     <ScrollBar orientation="horizontal" />
   </ScrollArea>
 </template>
+
+<style>
+/* NOTE(aguschin): for whatewer reason tailwind's col-span-* do not work, but this works */
+.colspan7 {
+  grid-column: span 7 / span 7;
+}
+.colspan6 {
+  grid-column: span 6 / span 6;
+}
+.colspan5 {
+  grid-column: span 5 / span 5;
+}
+.colspan4 {
+  grid-column: span 4 / span 4;
+}
+.colspan3 {
+  grid-column: span 3 / span 3;
+}
+.colspan2 {
+  grid-column: span 2 / span 2;
+}
+.colspan1 {
+  grid-column: span 1 / span 1;
+}
+</style>
