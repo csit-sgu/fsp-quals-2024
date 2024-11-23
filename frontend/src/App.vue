eced84a45f441245885d1ef65359467a368c9334
Michael Chernigin <chernigin@altlinux.org>
2024-11-23 12:14:49 +0400

    wip(frontend): fixed sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,6 +14,7 @@ import { WeeklyView, DateFilterPicker, Chooser } from '@/components/ui'
 import { ref } from 'vue'
 
 import { sports, countries, getRegions, getLocalities, countryHasRegions } from '@/lib/dataSource'
+import SidebarFooter from './components/ui/sidebar/SidebarFooter.vue'
 
 const route = useRoute()
 
@@ -53,51 +54,30 @@ const updateLocality = (newValue: string) => (pickedLocality.value = newValue)
 
 <template>
   <SidebarProvider>
-    <Sidebar collapsible="offcanvas">
+    <Sidebar collapsible="offcanvas" class="sticky top-0 h-screen">
       <SidebarContent>
         <SidebarGroup class="content-center px-4 w-auto">
           <SidebarGroupLabel class="pt-4 pb-6">Фильтрация по дате</SidebarGroupLabel>
           <DateFilterPicker @update="updateViewMode" />
           <SidebarGroupLabel class="pt-8 pb-6">Фитрация по соревнованиям</SidebarGroupLabel>
-          <Chooser
-            :options="sports"
-            :show-search="true"
-            default-msg="Любой вид спорта"
-            @update="updateSport"
-          />
+          <Chooser :options="sports" :show-search="true" default-msg="Любой вид спорта" @update="updateSport" />
           <SidebarGroupLabel class="pt-8 pb-6">Фитрация по месту проведения</SidebarGroupLabel>
-          <Chooser
-            :options="countries"
-            :show-search="true"
-            default-msg="Любая страна"
-            @update="updateCountry"
-          />
+          <Chooser :options="countries" :show-search="true" default-msg="Любая страна" @update="updateCountry" />
           <div v-if="countryHasRegions(pickedCountry)" class="pb-2" />
-          <Chooser
-            v-if="countryHasRegions(pickedCountry)"
-            :show-search="true"
-            :options="pickedCountryRegions"
-            default-msg="Любой регион"
-            @update="updateRegion"
-          />
+          <Chooser v-if="countryHasRegions(pickedCountry)" :show-search="true" :options="pickedCountryRegions"
+            default-msg="Любой регион" @update="updateRegion" />
           <div class="pt-2" />
-          <Chooser
-            v-if="
-              pickedRegion.length > 0 ||
-              (!countryHasRegions(pickedCountry) && pickedCountry.length > 0)
-            "
-            :show-search="true"
-            :options="pickedRegionLocalities"
-            default-msg="Любой населённый пункт"
-            @update="updateLocality"
-          />
+          <Chooser v-if="
+            pickedRegion.length > 0 ||
+            (!countryHasRegions(pickedCountry) && pickedCountry.length > 0)
+          " :show-search="true" :options="pickedRegionLocalities" default-msg="Любой населённый пункт"
+            @update="updateLocality" />
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
     <SidebarInset class="min-h-screen overflow-x-hidden">
       <header
-        class="flex w-full h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
-      >
+        class="flex w-full h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
         <div class="flex items-center gap-2 px-4">
           <SidebarTrigger class="-ml-1" />
           <Separator orientation="vertical" class="mr-2 h-4" />
