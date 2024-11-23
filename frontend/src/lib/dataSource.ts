f630aa34f0152c0c076b9f64f4efa6d2f3cd119e
Michael Chernigin <chernigin@altlinux.org>
2024-11-23 13:47:32 +0400

    feat(frontend): implement getting events from backend

--- frontend/src/lib/dataSource.ts
+++ frontend/src/lib/dataSource.ts
@@ -3,6 +3,61 @@ import { toCapitalCase } from './utils'
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ?? 'localhost:3000'
 
+export type Condition = {
+  additional_info?: string,
+  age?: number,
+  code?: string,
+  country?: string,
+  date_range?: {
+    from?: string,
+    to?: string
+  },
+  gender?: string,
+  locality?: string,
+  region?: string,
+  sport?: string,
+  stage?: string
+}
+
+export type Event = {
+  code: string,
+  start_date: string,
+  location_data: {
+    country: string,
+    region: string,
+    locality: string
+  }
+  age_data: {
+    gender: string,
+    left_bound: number,
+    right_bound: number,
+    original: string
+  },
+  title: string,
+  additional_info: string,
+  n_participants: number,
+  stage: string,
+  end_date: string,
+  sport: string
+}
+
+export let getEvents = async (
+  page: number,
+  page_size: number,
+  condition: Condition = {},
+  required_fields: string[] = []
+) => await axios
+  .post(BACKEND_URL + '/filter',
+    {
+      condition,
+      pagination: {
+        page,
+        page_size
+      },
+      required_fields
+    })
+  .then((res) => res.data)
+
 export const countryHasRegions = (country: string) => country == 'РОССИЯ'
 
 export let sports = await axios
