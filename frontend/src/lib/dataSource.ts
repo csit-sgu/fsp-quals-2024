ce843aca1243674f9cbb80354a888e861bf62a93
Michael Chernigin <chernigin@altlinux.org>
2024-11-24 08:56:02 +0400

    fix(frontend): fix sport and gender fields

--- frontend/src/lib/dataSource.ts
+++ frontend/src/lib/dataSource.ts
@@ -71,6 +71,11 @@ export const scaleMap = new Map<string, string>([
   ['Междурегионный', "interregional"],
 ])
 
+export const sexMap = new Map<string, string>([
+  ['Мужской', "male"],
+  ['Женский', "female"],
+])
+
 axios.interceptors.request.use(request => {
   console.log('Starting Request', JSON.stringify(request, null, 2))
   return request
